7bcc2bf63359fcd3edc92d3ab7dc9b63401ad74d
Jeff Hedfors <git.j@hedfors.net>
2016-04-30 18:10:56 -0700

    update

--- application/controllers/dashboard.php
+++ application/controllers/dashboard.php
@@ -3,7 +3,7 @@
 class Dashboard extends CI_Controller {
 	public function __construct(){
 		parent:: __construct();
-		$this->load->model('dashboard_model');
+		$this->load->model('Dashboard_model');
 	}
 	public function index()
 	{
